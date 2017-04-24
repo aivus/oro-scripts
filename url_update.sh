5cf6f5ea782529aaaba5fa73b27acf8d5a200c50
Ilya Antipenko <ilya@antipenko.pp.ua>
2017-04-24 10:45:25 +0300

    Add url_update.sh

---
+++ url_update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# OroCommerce script
+# This script updates configs to change OroCommerce url after installation
+#
+# You need to be in application directory of OroCommerce
+#
+# Usage: ./url_update.sh 0c747ef5.eu.ngrok.io
+
+php app/console oro:config:update oro_ui.application_url http://$1
+php app/console oro:config:update oro_website.url http://$1
+php app/console oro:config:update oro_website.secure_url https://$1
+
+echo "Application urls updated"
