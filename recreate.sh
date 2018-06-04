976aa7df089ada0494c7e1465b9325ffc7a62e7a
Ilya Antipenko <ilya@antipenko.pp.ua>
2018-06-04 11:05:17 +0300

    Add recreate script to recreate DB and flush installed flag

---
+++ recreate.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# OroCommerce script
+# This script recreates DB, create DB extension and reset installed flag
+#
+# You need to be in application directory of OroCommerce
+#
+# Usage: ./recreate.sh bap
+
+DB=${1}
+
+dropdb ${DB}
+echo "[/] [${DB}] dropped"
+
+createdb ${DB}
+echo "[-] [${DB}} created"
+
+psql -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";' -d ${DB} > /dev/null
+echo "[\] [${DB}} extension uuid-ossp created"
+
+sed -i '' "s/installed"\:".*/installed"\:" null/g" app/config/parameters.yml
+echo "[-] Flag \"installed\" flushed"
+echo "[+] Done"
