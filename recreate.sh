590e3dd36f559c7a4de0d63ee504318ea5bc02ba
Ilya Antipenko <ilya@antipenko.pp.ua>
2018-07-12 13:48:00 +0300

    Update recreate.sh to work with SF4 directory structure

--- recreate.sh
+++ recreate.sh
@@ -12,6 +12,7 @@ DB=${1}
 
 echo "[-] Removing cache"
 rm -rf app/cache/*
+rm -rf var/cache/*
 echo "[\] Cache removed"
 
 dropdb ${DB}
@@ -24,5 +25,6 @@ psql -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";' -d ${DB} > /dev/null
 echo "[\] [${DB}] extension uuid-ossp created"
 
 sed -i '' "s/installed"\:".*/installed"\:" null/g" app/config/parameters.yml
+sed -i '' "s/installed"\:".*/installed"\:" null/g" config/parameters.yml
 echo "[/] Flag \"installed\" flushed"
 echo "[+] Done"
